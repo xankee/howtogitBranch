f9b0a038d36a979f0120d5bf756521b729fe1e2b
Эмиль Дасаев 1101о <emil_dasaev@mail.ru>
2023-10-11 17:07:50 +0300

    task1

---
+++ src/calculatorgas.js
@@ -0,0 +1,5 @@
+const rashod = 50
+const distanse = 21
+const gasprice = 12
+const priceFor100KM =(gasprice * rashod);
+const doroga = (gasprice / distanse);
---
+++ src/ploshad.js
@@ -0,0 +1,6 @@
+const a = 5;
+const b = 2;
+const c = 2;
+let p = (a+b+c) / 2.0;
+let S = Math.sqrt(p * (p - a) * (p - b) * (p -c));
+console.log (S);
